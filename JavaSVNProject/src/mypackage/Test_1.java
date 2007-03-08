60f487aec95b1cb3ef96aa111b2bd60afeb0cb9d
suhel.hammoud <suhel.hammoud@73375f9a-2a2b-0410-9b5d-bff0e3eceac0>
2007-03-08 14:26:14 +0000

    Initial import.

---
+++ JavaSVNProject/src/mypackage/Test_1.java
@@ -0,0 +1,14 @@
+package mypackage;
+
+public class Test_1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("JAVA SVN DONE !");
+
+	}
+
+}
