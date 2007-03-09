db31371e035efbca31941fb80930f3270c137aa2
Suhel.hammoud <Suhel.hammoud@73375f9a-2a2b-0410-9b5d-bff0e3eceac0>
2007-03-09 08:45:19 +0000

    Added to the Branch

---
+++ Tags/JavaSVNProject/src/mypackage/Test_3.java
@@ -0,0 +1,14 @@
+package mypackage;
+
+public class Test_3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Class_3 Done !");
+
+	}
+
+}
